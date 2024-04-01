6b9dc7f00cc1a2c700302ce7b0368fd9bc5a1b87
NelsonGrajales <ngrajales2332@gmail.com>
2024-03-31 19:20:07 -0500

    practica 2

--- Practica #1/escena/personaje.gd
+++ Practica #1/escena/personaje.gd
@@ -1,17 +1,16 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var sprite_2d = $AnimatedSprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
-	# Add the gravity.
+	if Input.is_action_just_pressed("attack"):
+		sprite_2d.animation = "attack"
+		
 	if(velocity.x > 1 || velocity.x < -1):
 		sprite_2d.animation = "correr"
 	else:
@@ -21,12 +20,9 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		sprite_2d.animation = "saltar"
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
